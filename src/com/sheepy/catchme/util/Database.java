0753d6fc3b96be74754133a7d4059f172400a76d
Icyscools <meet.ice1@gmail.com>
2019-11-19 02:50:30 +0700

    Add MongoDB Util

--- src/com/sheepy/catchme/util/Database.java
+++ src/com/sheepy/catchme/util/Database.java
@@ -1,19 +1,48 @@
 package com.sheepy.catchme.util;
 
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 
 public class Database {
-	
-	private String ipaddr;
+
+	private String ipaddr; 
 	private int port;
-	private MongoClient db;
-	
+	private MongoClient mongoClient;
+
+	public Database() throws UnknownHostException {
+		this("127.0.0.1", 27017);
+	}
+
+	public Database(String ipaddr) throws UnknownHostException {
+		this(ipaddr, 27017);
+	}
+
 	public Database(String ipaddr, int port) throws UnknownHostException {
 		this.ipaddr = ipaddr;
 		this.port = port;
-		this.db = new MongoClient(ipaddr, port);
+		this.mongoClient = new MongoClient(ipaddr, port);
+		System.out.println("Connection Established");
+	}
+	
+	public String getConnectionIp() {
+		return this.ipaddr;
+	}
+	
+	public int getConnectionPort() {
+		return this.port;
+	}
+
+	public DB getDatabase(String dbName) {
+		return this.mongoClient.getDB(dbName);
+	}
+
+	public DBCollection getCollection(DB db, String collectionName) {
+		return db.getCollection(collectionName);
 	}
 
 }

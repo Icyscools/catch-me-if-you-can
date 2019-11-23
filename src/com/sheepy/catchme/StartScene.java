9e6719f65aef01ded6c88fe755f9300f06b292ad
ckrittima <42972920+ckrittima@users.noreply.github.com>
2019-11-24 00:56:29 +0700

    edit startscene

--- src/com/sheepy/catchme/StartScene.java
+++ src/com/sheepy/catchme/StartScene.java
@@ -8,24 +8,35 @@ import javax.swing.*;
 public class StartScene extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private JFrame fr;
-	private JButton bn1, bn2;
+	private JButton bn1, bn2, bn3;
+        private JPanel p0;
 	public StartScene() {
 		fr = new JFrame(Game.TITLE);
 		fr.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
 		this.setLayout(new GridBagLayout());
-		bn1 = new JButton("Start");
+                p0 = new JPanel();
+                p0.setLayout(new GridLayout(5,1));
+		bn1 = new JButton("New Game");
+                bn2 = new JButton("Join Game");
+                bn3 = new JButton("How To Play");
+                
 		bn1.setFont(new Font("TimesRoman", Font.BOLD, 20));
-		bn1.setBorder(null);
-		bn1.setBackground(Color.cyan);
+                bn2.setFont(new Font("TimesRoman", Font.BOLD, 20));
+                bn3.setFont(new Font("TimesRoman", Font.BOLD, 20));
 		
-                this.add(bn1);
+                p0.add(bn1);
+//                System.out.println("add1");
+                p0.add(new JLabel());
+                p0.add(bn2);
+                p0.add(new JLabel());
+                p0.add(bn3);
+//                System.out.println("add2");
+                this.add(p0);
 		fr.add(this);
 		fr.setSize(640, 640);
 		fr.setResizable(false);
 		fr.setVisible(true);
-		this.revalidate();
-		this.repaint();
 	}
 	
 	@Override
@@ -36,16 +47,16 @@ public class StartScene extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         
-        try {
+            try {
 			BufferedImage img = ImageIO.read(getClass().getResource("image/game_start.png"));
 			g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), null);
 			System.out.println(1);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-        
+		}  
 	}
+        
 	
 	public static void main(String[] args) {
 		new StartScene();
